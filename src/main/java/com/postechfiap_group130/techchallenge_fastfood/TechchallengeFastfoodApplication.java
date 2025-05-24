c74d6a6271a484af0fd6490b664d9832bfe86666
Denise Ferreira <ferreiras.denise@hotmail.com>
2025-05-24 16:21:45 -0300

    config

---
+++ src/main/java/com/postechfiap_group130/techchallenge_fastfood/TechchallengeFastfoodApplication.java
@@ -0,0 +1,13 @@
+package com.postechfiap_group130.techchallenge_fastfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechchallengeFastfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechchallengeFastfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/postechfiap_group130/techchallenge_fastfood/TechchallengeFastfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postechfiap_group130.techchallenge_fastfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechchallengeFastfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

225b049f10b90392b77ca37efca9bac819d8fe92
Thiago Frozzi <thiagofrozzi@hotmail.com>
2025-08-05 15:16:48 -0300

    ajuste no import

--- src/main/java/com/postechfiap_group130/techchallenge_fastfood/core/controllers/OrderController.java
+++ src/main/java/com/postechfiap_group130/techchallenge_fastfood/core/controllers/OrderController.java
@@ -1,6 +1,5 @@
 package com.postechfiap_group130.techchallenge_fastfood.core.controllers;
 
-import com.postechfiap_group130.techchallenge_fastfood.application.dtos.OrderRequestDto;
 import com.postechfiap_group130.techchallenge_fastfood.core.dtos.OrderResponseDto;
 import com.postechfiap_group130.techchallenge_fastfood.core.gateways.ProductGateway;
 import com.postechfiap_group130.techchallenge_fastfood.api.rest.dto.request.OrderRequestDto;
